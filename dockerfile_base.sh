b9efd2cfa9664646ea247ef7e4d06651b5f404e3
budtmo <budtmo.os@gmail.com>
2023-02-01 12:36:23 +0100

    Formatted

--- dockerfile_base.sh
+++ dockerfile_base.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 if [ -z "${1}" ]; then
-    read -p "Action (get|replace) : " ACTION
+	read -p "Action (get|replace) : " ACTION
 else
-    ACTION=${1}
+	ACTION=${1}
 fi
 
 function get() {
 	if [ -z "${1}" ]; then
-	    read -p "Dockerfile path : " path
+		read -p "Dockerfile path : " path
 	else
-	    path=${1}
+		path=${1}
 	fi
-	
+
 	if [ -f "${path}" ]; then
-	    base_img=$(cat ${path} | grep FROM | cut -d' ' -f2)
+		base_img=$(cat ${path} | grep FROM | cut -d' ' -f2)
 		echo ${base_img}
 	else
 		echo "${path} doesnt exist. Please check again!"
@@ -24,27 +24,27 @@ function get() {
 
 function replace() {
 	if [ -z "${1}" ]; then
-	    read -p "old image : " old
+		read -p "old image : " old
 	else
-	    old=${1}
+		old=${1}
 	fi
 
 	if [ -z "${2}" ]; then
-	    read -p "new image : " new
+		read -p "new image : " new
 	else
-	    new=${2}
+		new=${2}
 	fi
 
 	if [ -z "${3}" ]; then
-	    read -p "Dockerfile path : " path
+		read -p "Dockerfile path : " path
 	else
-	    path=${3}
+		path=${3}
 	fi
 
 	echo ${old} ${new}
 
 	if [ -f "${path}" ]; then
-	    sed -i "s/${old}/${new}/g" ${path}
+		sed -i "s/${old}/${new}/g" ${path}
 	else
 		echo "${path} doesnt exist. Please check again!"
 		exit -1
@@ -52,13 +52,13 @@ function replace() {
 }
 
 case ${ACTION} in
-    get)
-        get ${2}
-    ;;
-    replace)
-        replace ${2} ${3} ${4}
-    ;;
-    *)
-        echo "Invalid action! Valid options: get, replace"
-    ;;
+get)
+	get ${2}
+	;;
+replace)
+	replace ${2} ${3} ${4}
+	;;
+*)
+	echo "Invalid action! Valid options: get, replace"
+	;;
 esac
